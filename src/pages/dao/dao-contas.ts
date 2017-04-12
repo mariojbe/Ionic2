1457108766cc6d916adfac8318adb83e23984b8d
mariojbe <mariojbe@hotmail.com>
2017-04-12 15:09:41 -0300

    Revisando a Pasta DAO

---
+++ src/pages/dao/dao-contas.ts
@@ -0,0 +1,31 @@
+export class DAOContas {
+
+    list:any;
+
+    constructor(){
+        this.list = [];
+    }
+
+    getList(){
+        this.list = [
+            {descricao: "Alimentação"},
+            {descricao: "Lazer"},
+            {descricao: "Transporte"}
+        ];
+
+        return this.list;
+    }
+
+    insert(conta){
+
+    }
+
+    edit(conta){
+
+    }
+
+    delete(conta){
+
+    }
+
+}
